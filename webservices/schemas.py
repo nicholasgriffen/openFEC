4cbba0f3f1e79dc94eb169e88fb8070dcef077c6
Laura Beaufort <lbeaufort@fec.gov>
2020-07-28 10:39:32 -0400

    WIP - remove sort expression

--- webservices/schemas.py
+++ webservices/schemas.py
@@ -670,7 +670,7 @@ ScheduleBSchema = make_schema(
             'disbursement_description_text',
             'recipient_street_1',
             'recipient_street_2',
-            'sort_expressions',
+            #'sort_expressions',
         ),
         'relationships': [
             Relationship(
