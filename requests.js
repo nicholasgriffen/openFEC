2e11cc26f3ec6c1f76d9221ef0019dc032dc8ca8
Nick Griffen <nicholas.s.griffen@gmail.com>
2020-08-01 02:08:06 -0600

    add some consts

--- requests.js
+++ requests.js
@@ -1,2 +1,5 @@
+const baseApi = "https://api.open.fec.gov/v1/"
+const candidateSearch = "candidates/search/"
+const apiKey = "jLvePfRekLJlkciLf2UszC7LCaEb4vyD5SqiuFxE"
 const getBody = result => result.then(res => res.json()).then(x => resBody = x)
-const search = (term) => fetch('https://api.open.fec.gov/v1/candidates/search/?api_key=jLvePfRekLJlkciLf2UszC7LCaEb4vyD5SqiuFxE')
+const search = (endp) => fetch(`${baseApi}${candidateSearch}?api_key=${apiKey}`)
